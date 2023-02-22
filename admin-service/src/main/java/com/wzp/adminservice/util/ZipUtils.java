92cabc22d085e5661ccda766b7a3e49c41dcaf69
zp.wei <1348069215@qq.com>
2023-02-22 17:49:26 +0800

    fix: update

--- admin-service/src/main/java/com/wzp/adminservice/util/ZipUtils.java
+++ admin-service/src/main/java/com/wzp/adminservice/util/ZipUtils.java
@@ -16,22 +16,20 @@ public class ZipUtils {
     private static final int BUFFER_SIZE = 2 * 1024;
 
     /**
-     * 压缩成ZIP 方法     * @param srcDir 压缩文件夹路径
-     *
+     * 压缩成ZIP 方法
+     * @param srcDir 压缩文件夹路径
      * @param out              压缩文件输出流
-     * @param KeepDirStructure 是否保留原来的目录结构,true:保留目录结构;
-     *                         false:所有文件跑到压缩包根目录下(注意：不保留目录结构可能会出现同名文件,会压缩失败)
+     * @param keepDirStructure 是否保留原来的目录结构,true:保留目录结构;false:所有文件跑到压缩包根目录下(注意：不保留目录结构可能会出现同名文件,会压缩失败)
      * @throws RuntimeException 压缩失败会抛出运行时异常
      */
-    public static void toZip(String srcDir, OutputStream out, boolean KeepDirStructure)
-            throws RuntimeException {
+    public static void toZip(String srcDir, OutputStream out, boolean keepDirStructure) throws RuntimeException {
         log.info("正在压缩文件。。。");
         long start = System.currentTimeMillis();
         ZipOutputStream zos = null;
         try {
             zos = new ZipOutputStream(out);
             File sourceFile = new File(srcDir);
-            compress(sourceFile, zos, sourceFile.getName(), KeepDirStructure);
+            compress(sourceFile, zos, sourceFile.getName(), keepDirStructure);
             long end = System.currentTimeMillis();
             log.info("压缩完成，耗时：" + (end - start) + " ms");
         } catch (Exception e) {
@@ -49,8 +47,8 @@ public class ZipUtils {
     }
 
     /**
-     * 压缩成ZIP 方法     * @param srcFiles 需要压缩的文件列表
-     *
+     * 压缩成ZIP 方法
+     * @param srcFiles 需要压缩的文件列表
      * @param out 压缩文件输出流
      * @throws RuntimeException 压缩失败会抛出运行时异常
      */
